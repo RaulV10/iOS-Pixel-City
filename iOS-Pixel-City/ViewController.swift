5c6a9ff087c555c43e5108a6b126682e080f13a8
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-17 17:53:51 -0600

    Initial Commit

---
+++ iOS-Pixel-City/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Pixel-City
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/17/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
